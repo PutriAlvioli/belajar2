3f4f4e1008aba17cc1a2cfdd1550809bed925728
PutriAlvioli · she/her <putrialvioliputri@gmail.com>
2023-09-27 21:58:00 +0700

    first commit

---
+++ switch.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $X=2; switch ($X)
+        {
+            case 1:
+                echo "Nomor 1"; break; case 2 :
+                echo "Nomor 2"; break; case 3 :
+                echo "Nomor 3"; break; default:
+                echo "Bukan Nomor diantara 1 dan 3";
+        }
+    ?>
+</body>
+</html>
